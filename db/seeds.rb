4eacd25edc0607820bbd7cbb8a833eb1ca8e9515
Richard Jarram <rjarram@me.com>
2019-02-25 19:18:57 -0500

    merged seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Task.destroy_all
+Goal.destroy_all
+User.destroy_all
+Category.destroy_all
+
+
+puts 'Creating users...'
+category_employment = Category.create!(name: 'Employment', colour: 'blue')
+fouzia = User.create!(first_name: 'Fouzia', last_name: 'Al Hashish', age: 33, nationality: 'Syrian', language: 'Arabic', address: 'Toronto, Ontario', email: 'f.al_hashish@yahoo.fr', password: 'f.al_hashish@yahoo.fr')
+nooshin = User.create!(first_name: 'Nooshin', last_name: 'Amineh', age: 25, nationality: 'Iranian', language: 'Persan', address: 'Halifax, Nova-Scotia', email: 'n.amineh@hotmail.com', password: 'n.amineh@hotmail.com')
+axmed = User.create!(first_name: 'Axmed', last_name: 'Nuur', age: 39, nationality: 'Somalian', language: 'Arabic', address: 'Montreal, Quebec', email: 'a.nuur@gmail.com', password: 'a.nuur@gmail.com')
+goal_employment = Goal.create!(name: 'Employment', category: category_employment, user: fouzia, completed: false)
+
+
+Task.create!(goal: goal_employment, title: 'DSH appointment', details: 'bljaljlfkjgla', deadline: '30 days', completed: false, address: 'Montreal')
+Task.create!(goal: goal_employment, title: 'Employment appointment', details: 'bljaljlfkjgla', deadline: '12 days', completed: false, address: 'Montreal')
+Task.create!(goal: goal_employment, title: 'Medical appointment', details: 'bljaljlfkjgla', deadline: '45 days', completed: true, address: 'Montreal')
+
+
+puts 'Finished!'
