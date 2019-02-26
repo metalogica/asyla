78c7ee4d89f0dbf20005dc8bc601e9d30d9190c2
Richard Jarram <rjarram@me.com>
2019-02-26 14:53:41 -0500

    completed links between all pages and edited devise log in functionality. user must be loggin in to view app.

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,6 +1,4 @@
 class RecordsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show] #Delete this when we build devise functionality.
-
   def index
   end
 
