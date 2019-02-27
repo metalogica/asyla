9af354cde3ef351eb54670c595c4821f9fbd3db1
Emilie Morissette <emorissettegregoire@gmail.com>
2019-02-27 14:12:00 -0500

    Create array of goal hashes in the Goal controller

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -2,11 +2,56 @@ class GoalsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show] #Delete this when we build devise functionality.
 
   def index
-    # I want to get goals for a specific user for displaying them
+    # I want to get goals for a specific user to be able to display them
     @goals = Goal.where(user: current_user)
+    # I want to count the number of completed tasks for each goal
+    @dashboard = dashboard_hash
   end
+
+  def dashboard_hash
+    dashboard = []
+
+    @goals.each do |goal|
+      goals_hash = {
+        goal_name: goal.name,
+        total_tasks: goal.tasks.size,
+        completed_tasks: count_tasks_completed,
+        progress: count_tasks_completed / goal.tasks.size
+      }
+
+      dashboard << goals_hash
+    end
+    dashboard
+  end
+
+
+  def count_tasks_completed
+    @goals.each do |goal|
+      completed_tasks = goal.tasks.select { |task| task.completed == true }
+      return completed_tasks.count
+    end
+  end
+
+  # def count_total_tasks_completed_per_goal
+  #   count = 0
+  #   @goals.each do |goal|
+  #     goal.tasks.each do |task|
+  #       count += 1 if task.completed == true
+  #     end
+  #   end
+  #   return count
+  # end
+
+  # def count_total_tasks_per_goal
+  #   @goals.each do |goal|
+  #     total = goal.tasks.size
+  #   end
+  #   return total
+  # end
+
 
   def show
     # I want to show the specific tasks for a particular goal
   end
 end
+
--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -8,4 +8,5 @@ class RecordsController < ApplicationController
   def show
     @record = Record.find(params[:id])
   end
+
 end
