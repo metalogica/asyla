79a9641c73d0bfa8db52a0621f878a399bf8c802
Richard Jarram <rjarram@me.com>
2019-02-27 17:43:59 -0500

    bugs

--- db/seeds.rb
+++ db/seeds.rb
@@ -45,9 +45,9 @@ Goal.all.each do |goal|
   end
 end
 
-date1 = Time.new(2019,2,5);
-date2 = Time.new(2019,3,8);
-date3 = Time.new(2019,3,8);
+# date1 = Time.new(2019,2,5);
+# date2 = Time.new(2019,3,8);
+# date3 = Time.new(2019,3,8);
 
 # Task.create!(goal: goal_employment, title: 'DSH appointment', details: 'bljaljlfkjgla', deadline: date1, completed: false, address: 'Montreal')
 # Task.create!(goal: goal_employment, title: 'Employment appointment', details: 'bljaljlfkjgla', deadline: date2, completed: false, address: 'Montreal')
