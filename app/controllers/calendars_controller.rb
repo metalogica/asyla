b341da1fcea8fc9c4abe6394620fe4613026c909
Richard Jarram <rjarram@me.com>
2019-02-27 21:03:39 -0500

    attempt to integrate calendar

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,6 +1,11 @@
 class CalendarsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:show]
 
+  def index # RJ added this controller when trying to integrate calendar into app.
+    @goals = Goal.where(user: current_user)
+    @tasks = Task.where(user: current_user)
+  end
+
   def show
     @date = params[:id] || Time.now
     @tasks = Task.all
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :calendars, only: [:show]
+  resources :calendars, only: [:index, :show] # RJ added index here when trying to integrate calendar into app.
   resources :goals, only: [:index, :show]
   resources :tasks, only: [:index, :show]
   resources :records, only: [:index, :show]
 
-  root to: 'tasks#index' # Will change this to login page later.
+  root to: 'goals#index' # Will change this to login page later.
 end
