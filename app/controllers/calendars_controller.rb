29546fce0775eda72cad66609d9bcf375833cb5c
Richard Jarram <rjarram@me.com>
2019-02-27 20:54:06 -0500

    built navbar after merge

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,8 +2,7 @@ class CalendarsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:show]
 
   def show
-    @date = params[:id] || Time.now 
+    @date = params[:id] || Time.now
     @tasks = Task.all
-    
   end
 end
