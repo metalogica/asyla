5e8b75c84b208bc48480f91c5ffcf594cffa9b55
Richard Jarram <rjarram@me.com>
2019-02-28 09:54:32 -0500

    integrated the settins controller nd edited hte records page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
+
+  resources :settings, only: [:index]
   resources :calendars, only: [:index, :show] # RJ added index here when trying to integrate calendar into app.
   resources :goals, only: [:index, :show]
   resources :tasks, only: [:index, :show]
