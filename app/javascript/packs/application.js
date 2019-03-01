feece0e9df6c7a971ae866c76cd7a95debbf8662
Richard Jarram <rjarram@me.com>
2019-03-01 16:43:25 -0500

    final style changes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,10 @@
 import "bootstrap";
-import './alert_buttons';
-import "./carousel";
+// import "./alert_buttons";
 import "./index.js"
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 
 import { initMapbox } from '../plugins/init_mapbox';
 
 initMapbox();
+// cancellationAlert();
 
