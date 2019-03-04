00e0999843e45b42a68019bc8dd4f414d8a43cda
Jamie Webber <jamiecwebber@gmail.com>
2019-03-04 10:37:07 -0500

    added next_five_tasks to calendar helper

--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -7,24 +7,6 @@ module CalendarHelper
     date.year
   end
 
-  def tasks_hash(date, tasks)
-    tasks_hash = {}
-    tasks.each do |task|
-      deadline = task.deadline
-      puts deadline
-      puts date
-      if deadline.month == date.month && deadline.year == date.year
-        if tasks_hash.key?(deadline.day)
-          tasks_hash[deadline.day] << task
-        else
-          tasks_hash[deadline.day] = [task]
-        end
-      end
-    end
-
-    return tasks_hash
-  end
-
   def days_array(date)
     days_array = (1..Time.days_in_month(date.month)).to_a
     Time.new(date.year, date.month, 1).wday.times { days_array.unshift("") }
@@ -41,4 +23,29 @@ module CalendarHelper
     end
     return classes
   end
+
+  def tasks_hash(date, tasks)
+    tasks_hash = {}
+    tasks.each do |task|
+      deadline = task.deadline
+      if deadline.month == date.month && deadline.year == date.year
+        if tasks_hash.key?(deadline.day)
+          tasks_hash[deadline.day] << task
+        else
+          tasks_hash[deadline.day] = [task]
+        end
+      end
+    end
+    return tasks_hash
+  end
+
+  def next_five_tasks(tasks)
+    next_five = tasks.to_a.sort {| task1, task2 | task1.deadline <=> task2.deadline }
+    next_five.first(5).map! do |task| 
+      days_away = (task.deadline - DateTime.today).to_i
+      return [task, days_away]
+    end
+    return next_five
+  end
+
 end
