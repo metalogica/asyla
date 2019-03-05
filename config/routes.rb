58a19ef658cd587f14d93b985e710be4fa23eb99
Jamie Webber <jamiecwebber@gmail.com>
2019-03-05 17:38:05 -0500

    animated progress circles, task info show-hide, changed colours of next 5 tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
 
+  resources :users, except: :create
+  post 'create_user' => 'users#create', as: :create_user
+
   resources :settings, only: [:index]
   resources :calendars, only: [:index, :show] # RJ added index here when trying to integrate calendar into app.
   resources :records, only: [:index, :show]
