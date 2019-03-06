f84583858ee5effe85b9f4ad1711f35c586f3aeb
Emilie Morissette <emorissettegregoire@gmail.com>
2019-03-06 16:42:58 -0500

    bell toggleling but not synchronized with the notification bar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,7 @@ import './index.js'
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import { initMapbox } from '../plugins/init_mapbox';
 import {bellToggle} from './bell-notification-toggle';
+bellToggle()
 initMapbox();
 
 
--- app/javascript/packs/bell-notification-toggle.js
+++ app/javascript/packs/bell-notification-toggle.js
@@ -1,7 +1,12 @@
 export const bellToggle = () => {
   const bellFill = document.getElementById('bell-fill');
-  const bellEmpty = document.querySelector('.bell');
+  const bellEmpty = document.getElementById('bell');
   bellEmpty.addEventListener("click", (event) => {
     bellFill.classList.toggle('hidden');
+    bellEmpty.classList.toggle('hidden')
+  });
+   bellFill.addEventListener("click", (event) => {
+    bellFill.classList.toggle('hidden');
+    bellEmpty.classList.toggle('hidden')
   });
 }
