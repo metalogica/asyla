0746ede4fa4a289b786192618ea43ff5a99e13ef
Richard Jarram <rjarram@me.com>
2019-03-06 09:47:48 -0500

    finisghed js for alert button

--- app/javascript/packs/alert-toggle.js
+++ app/javascript/packs/alert-toggle.js
@@ -1,1 +1,7 @@
-const alertButton = document.querySelector('.sidebar');
+export const alertToggle = () => {
+  const alertIcon = document.getElementById('bell');
+  const notificationBar = document.querySelector('.notifications-bar');
+  alertIcon.addEventListener("click", (event) => {
+    notificationBar.classList.toggle('hidden');
+  });
+}
