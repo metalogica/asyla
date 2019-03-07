f1741e18cc3d7a5b6be5025db3800394dd729013
Emilie Morissette <emorissettegregoire@gmail.com>
2019-03-07 10:19:48 -0500

    empty bell synchronize with notification bar

--- app/javascript/packs/alert-toggle.js
+++ app/javascript/packs/alert-toggle.js
@@ -1,10 +1,28 @@
-export const alertToggle = () => {
+const alertToggle = () => {
   const alertIcon = document.getElementById('bell-fill');
   const notificationBar = document.querySelector('.notifications-bar');
   const notificationTitle = document.querySelector('.notifications-title');
-
-  alertIcon.addEventListener("click", (event) => {
     notificationBar.classList.toggle('hidden');
     notificationTitle.classList.toggle('hidden');
-  });
 }
+
+const bellToggle = () => {
+  const bellFill = document.getElementById('bell-fill');
+  const bellEmpty = document.getElementById('bell');
+    bellFill.classList.toggle('hidden');
+    bellEmpty.classList.toggle('hidden');
+}
+
+
+export const notifications = () => {
+  document.getElementById('bell-fill').addEventListener('click', function(){
+    alertToggle();
+    bellToggle();
+  });
+  document.getElementById('bell').addEventListener('click', function(){
+    alertToggle();
+    bellToggle();
+  });
+};
+
+// mouse enter/mouse leave
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,12 +1,10 @@
-import {alertToggle} from './alert-toggle';
-alertToggle();
+import {notifications} from './alert-toggle';
+notifications();
 import 'bootstrap';
 import './alert-buttons';
 import './index.js'
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import { initMapbox } from '../plugins/init_mapbox';
-import {bellToggle} from './bell-notification-toggle';
-bellToggle()
 initMapbox();
 
 
