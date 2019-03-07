f3091c87b58a9e5fae0c03ad13467d4a89511076
Emilie Morissette <emorissettegregoire@gmail.com>
2019-03-07 11:10:20 -0500

    done

--- app/javascript/packs/alert-toggle.js
+++ app/javascript/packs/alert-toggle.js
@@ -8,16 +8,16 @@ const alertToggle = () => {
 const bellToggle = () => {
   const bellFill = document.getElementById('bell-fill');
   const bellEmpty = document.getElementById('bell');
-    // bellFill.classList.toggle('hidden');
+    bellFill.classList.toggle('hidden');
     bellEmpty.classList.toggle('hidden');
 }
 
 export const notifications = () => {
   document.getElementById('bell-fill').addEventListener('click', function(){
     alertToggle();
-    bellToggle();
+    // bellToggle();
   });
-  document.getElementById('bell-fill').addEventListener('click', function(){
+  document.getElementById('bell').addEventListener('click', function(){
     bellToggle();
   });
 };
