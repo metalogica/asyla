c7ecbd3e27e1319db433069374c51cbf296a0035
Richard Jarram <rjarram@me.com>
2019-05-23 21:36:31 -0400

    added client credentials to login screen

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,12 +1,13 @@
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import { initMapbox } from '../plugins/init_mapbox';
-initMapbox();
 import {notifications} from './alert-toggle';
-notifications();
 import {bellHover} from './alert-toggle';
-bellHover();
 import 'bootstrap';
 import './alert-buttons';
 import './index.js'
+
+initMapbox();
+notifications();
+bellHover();
 
 
